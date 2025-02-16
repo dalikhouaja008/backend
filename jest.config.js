71aba25cdc55307be223e787dbadbb1b5be85e1a
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2025-02-16 16:45:28 +0100

    modifer package.json

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: '.',  // Changed from 'src' to '.' to handle all project files
+    rootDir: '.',
     testRegex: '.*\\.spec\\.ts$',
     transform: {
       '^.+\\.(t|j)s$': 'ts-jest'
