de7959747e3b09cdc595f4a00b451d3583c02edf
dalikhouaja008 <mohamedali.khouaja@esprit.tn>
2025-02-16 16:40:45 +0100

    modifer package.json

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,19 @@
 module.exports = {
+    moduleFileExtensions: ['js', 'json', 'ts'],
+    rootDir: '.',  // Changed from 'src' to '.' to handle all project files
+    testRegex: '.*\\.spec\\.ts$',
+    transform: {
+      '^.+\\.(t|j)s$': 'ts-jest'
+    },
     collectCoverageFrom: [
       'src/**/*.ts',
-      '!src/**/*.spec.ts',
-      '!src/**/*.e2e-spec.ts',
+      '!src/**/*.module.ts',
       '!src/main.ts',
-      '!src/**/*.module.ts'
+      '!src/**/*.d.ts'
     ],
-    coveragePathIgnorePatterns: [
-      'node_modules',
-      'test-config',
-      'interfaces',
-      'jestGlobalMocks.ts',
-      '.module.ts',
-      '.mock.ts'
-    ],
-    // Add configuration for finding related tests
-    testMatch: ['**/*.spec.ts'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src/'],
     moduleNameMapper: {
       '^src/(.*)$': '<rootDir>/src/$1'
     }
